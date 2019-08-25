e75460a855d24adcb5abbabb3287a7094a423fb4
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-25 10:17:32 +0530

    Text rendering changed.

--- States/TitleState.lua
+++ States/TitleState.lua
@@ -2,7 +2,7 @@ TitleState = Class{__includes = BaseState}
 
 function TitleState:init()
     self.heading = "Flappy Bird"
-    self.message = "Press Space to Continue"
+    self.message = "Press Space to Start"
 end
 
 function TitleState:update()
@@ -12,9 +12,10 @@ function TitleState:update()
 end
 
 function TitleState:render()
+    love.graphics.setColor(245 / 255, 236 / 255, 66 / 255, 255 / 255)
     love.graphics.setFont(largeFont)
-    love.graphics.printf(self.heading, 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
-    love.graphics.setFont(smallFont)
-    love.graphics.printf(self.message, 0, VIRTUAL_HEIGHT - 16, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf(self.heading, 0, VIRTUAL_HEIGHT / 3 - 16, VIRTUAL_WIDTH, 'center')
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf(self.message, 0, VIRTUAL_HEIGHT / 1.5 - 16, VIRTUAL_WIDTH, 'center')
 
 end
