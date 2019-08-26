ba4f16e16ef821f58c7c589d5f73bd0f42757b3c
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-26 12:39:46 +0530

    Added comments and indentation, interchanged countdown state with play state as intermediate.

--- States/TitleState.lua
+++ States/TitleState.lua
@@ -1,18 +1,28 @@
 TitleState = Class{__includes = BaseState}
 
 function TitleState:init()
+
+    -- Initializing variables required for rendering
+    -- messages specific to title state
     self.heading = "Flappy Bird"
     self.message = "Press Space to Start"
 end
 
 function TitleState:update()
+
+    -- Changing state to countdown if space is pressed
     if love.keyboard.wasPressed('space') then
-        stateMachine:change('play')
+        stateMachine:change('countdown')
     end
+    
 end
 
 function TitleState:render()
+
+    -- Setting up kind of yellow color
     love.graphics.setColor(245 / 255, 236 / 255, 66 / 255, 255 / 255)
+
+    -- Rendering messages specific to title state
     love.graphics.setFont(largeFont)
     love.graphics.printf(self.heading, 0, VIRTUAL_HEIGHT / 3 - 16, VIRTUAL_WIDTH, 'center')
     love.graphics.setFont(mediumFont)
