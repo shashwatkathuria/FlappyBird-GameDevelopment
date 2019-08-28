b06260ce1f4f43b55a9b330be04cdb04710ddba6
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-28 13:06:56 +0530

    Added comments and indentation.

--- Classes/Bird.lua
+++ Classes/Bird.lua
@@ -1,6 +1,8 @@
 Bird = Class{}
 
 function Bird:init()
+
+    -- Initializing variables required
     self.image = love.graphics.newImage('Images/bird.png')
     self.height = self.image:getHeight()
     self.width = self.image:getWidth()
@@ -8,15 +10,18 @@ function Bird:init()
     self.x = VIRTUAL_WIDTH / 2 - self.width / 2
     self.y = VIRTUAL_HEIGHT / 2 - self.height / 2
     self.vy = 0
-    self.score = 0
+
 end
 
 function Bird:update(dt)
 
+    -- Update motion only if scrolling
     if scrolling then
-      
+
+        -- Updating motion
         self.vy = self.vy + GRAVITY * dt
 
+        -- Additional jump on pressing space
         if love.keyboard.wasPressed('space') then
             self.vy = -5
         end
@@ -27,12 +32,9 @@ function Bird:update(dt)
 
 end
 
--- function Bird:jump(dt)
---
---
---
--- end
-
 function Bird:render()
+
+    -- Rendering bird
     love.graphics.draw(self.image, self.x, self.y)
+
 end
