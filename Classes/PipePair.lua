3ed6ec959742812351003a734b6eb446d256fa00
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-29 16:57:52 +0530

    Added comments and indentation.

--- Classes/PipePair.lua
+++ Classes/PipePair.lua
@@ -1,6 +1,8 @@
 PipePair = Class{}
 
 function PipePair:init(bottomY, pipeGap)
+
+    -- Initializing variables required
     self.pipeGap = pipeGap
     self.bottomY = bottomY
     self.topPipe = Pipe('top', self.bottomY)
@@ -10,18 +12,25 @@ function PipePair:init(bottomY, pipeGap)
 
 end
 
+function PipePair:collides(bird)
+
+    -- Return true if either one collides
+    return self.bottomPipe:collides(bird) or self.topPipe:collides(bird, self.pipeGap)
+
+end
+
 function PipePair:update(dt)
+
+    -- Updating both top and bottom pipe
     self.topPipe:update(dt)
     self.bottomPipe:update(dt)
 
 end
 
 function PipePair:render(dt)
+
+    -- Rendering both top and bottom pipe
     self.topPipe:render()
     self.bottomPipe:render()
-end
 
-function PipePair:collides(bird)
-
-    return self.bottomPipe:collides(bird) or self.topPipe:collides(bird, self.pipeGap)
 end
